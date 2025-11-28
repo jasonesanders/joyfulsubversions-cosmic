4b6c4d3a493efbddc2d6c3a2936c547205265ab4
Jason Sanders <jasonsanders@admins-MacBook-Pro.local>
2025-11-27 21:02:19 -0800

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jasonesanders.github.io',
+  base: '/joyfulsubversions-cosmic',
   vite: {
     plugins: [tailwindcss()]
   }
